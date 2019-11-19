6d45204db88db68b8a7794100ce07761948d5473
khaleeqrh <khaleequr2@outlook.com>
2019-11-19 12:50:25 +0500

    validation of admin login screen

--- app/src/main/java/com/example/student_staff_card/Login.java
+++ app/src/main/java/com/example/student_staff_card/Login.java
@@ -6,17 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
-
+EditText eduser, edpassword;
     public Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
 
         setContentView ( R.layout.activity_login );
-
+        eduser=findViewById(R.id.user);
+        edpassword=findViewById(R.id.password);
         login= findViewById ( R.id.login );
 
 
@@ -26,8 +29,35 @@ public class Login extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
 
-                        Intent i = new Intent ( getApplicationContext (), Home.class );
-                        startActivity ( i );
+                        String username, password;
+                        username=eduser.getText().toString();
+                        password=edpassword.getText().toString();
+                        if((username.equals("") || username.equals(null)) && (password.equals("") || password.equals(null))){
+                            eduser.setError("Error");
+                            eduser.setBackgroundResource(R.drawable.edittext_error);
+                            edpassword.setError("Error");
+                            edpassword.setBackgroundResource(R.drawable.edittext_error);
+                        }
+                        else if(!username.equals("") || !username.equals(null) && (password.equals("") || password.equals(null))){
+
+                            edpassword.setError("Error");
+                            edpassword.setBackgroundResource(R.drawable.edittext_error);
+                            eduser.setBackgroundResource(0);
+                        }
+                        else if((username.equals("") || username.equals(null)) && (!password.equals("") || !password.equals(null))){
+                            edpassword.setBackgroundResource(0);
+                            eduser.setError("Error");
+                            eduser.setBackgroundResource(R.drawable.edittext_error);
+                        }
+                        else if(!username.equals("") && !username.equals(null) && !password.equals("") && !password.equals(null)){
+                            Intent i = new Intent ( getApplicationContext (), Home.class );
+                            startActivity ( i );
+                        }
+                        else
+                            {
+
+                            }
+
 
                     }
                 } );
